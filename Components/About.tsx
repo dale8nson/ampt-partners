169931a2965f901bb002e9ac30a0d97165f3d5f5
Dale Hutchinson <daletristanhutchinson@gmail.com>
2024-09-05 18:33:27 +1000

    more styling

--- Components/About.tsx
+++ Components/About.tsx
@@ -6,34 +6,32 @@ import Image from 'next/image'
 export const About = () => {
   return (
     <div className="py-16 px-16 bg-gray-900 mx-0 w-full lg:py-20">
-      <div className="m-auto sm:text-center lg:w-1/2">
+      <div className="flex flex-col m-auto sm:text-center lg:w-1/2">
         <div className="w-full mb-10 mx-auto sm:text-center md:mb-12">
           <div className="flex justify-center w-full mx-auto">
-            <motion.h2 {...{variants:scrollProps.variants, initial: scrollProps.initial, animate:"whileInView", transition: {duration:0.5, delay:1.3}}} className="lg:-translate-y-32 mb-6  md:text-5xl font-bold leading-none tracking-tight text-neutral-100 font-[masthead] text-4xl">
-                ABOUT
+            <motion.h2 {...{ variants: scrollProps.variants, initial: scrollProps.initial, animate: "whileInView", transition: { duration: 0.5, delay: 1.3 } }} className="lg:-translate-y-32 mb-6  md:text-5xl font-bold leading-none tracking-tight text-neutral-100 font-[masthead] text-4xl">
+              ABOUT
             </motion.h2>
           </div>
-          <motion.p {...{...scrollProps}} className="text-base text-neutral-100 w-full md:text-lg">
+          <motion.p {...{ ...scrollProps }} className="text-base text-neutral-100 w-full md:text-lg">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium doloremque rem aperiam, eaque ipsa quae.
           </motion.p>
         </div>
-        <div className="mb-4 transition-shadow duration-300 hover:shadow-xl lg:mb-6">
-          <motion.div {...scrollProps} className="w-full h-56 sm:h-64 md:h-80 lg:h-96">
-            <Image 
-            width={200} height={200} 
-              className="object-cover object-top w-full h-56 rounded shadow-lg sm:h-64 md:h-80 lg:h-96"
-              src="/S8_Louis_Litt.webp"
-              alt=""
-            />
-          </motion.div>
-        </div>
-        <motion.p {...{...scrollProps}} className="max-w-xl mb-4 text-base text-neutral-100 sm:mx-auto">
+        <motion.div {...scrollProps} className="w-full  lg:h-96">
+          <Image
+            width={200} height={200}
+            className="object-cover object-top w-full rounded shadow-lg h-[60vw] md:h-[60vw] lg:h-96"
+            src="/S8_Louis_Litt.webp"
+            alt=""
+          />
+        </motion.div>
+        <motion.p {...scrollProps} className="mb-4 text-base text-neutral-100 sm:mx-auto">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud ullamco laboris aliquip ex ea.
         </motion.p>
-        <motion.a {...{...scrollProps}} 
+        <motion.a {...{ ...scrollProps }}
           href="/"
           aria-label=""
           className="inline-flex items-center font-semibold transition-colors duration-200 text-neutral-100 hover:text-deep-purple-800"
