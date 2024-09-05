e7c3eed2729dcc2c1ec15cd70cde5e274a134302
Dale Hutchinson <daletristanhutchinson@gmail.com>
2024-09-05 17:21:33 +1000

    added fallback picture in header

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -8,7 +8,10 @@ export const Header = () => {
    return (
    <section className="relative flex-col h-[60vh] lg:h-[80vh] px-0 overflow-hidden w-full mx-0 z-[-10] justify-center align-middle">
       <div className='h-[60vh] lg:h-[80vh] w-full overflow-hidden'>
-        <video src='17920530-uhd_3840_2160_30fps copy.webm' className="fixed -z-50 px-0 mx-auto w-full h-[60vh] lg:h-[80vh] object-cover content-center" autoPlay loop muted />
+        <video  className="fixed -z-50 px-0 mx-auto w-full h-[60vh] lg:h-[80vh] object-cover content-center" autoPlay loop muted >
+          <source src='17920530-uhd_3840_2160_30fps copy.webm' />
+          <source src='/pexels-finn-whelen-6227601-6182776.jpg' />
+        </video>
       <Canvas className="absolute w-full !h-[125px] translate-y-40  mx-auto lg:translate-y-32 opacity-100 lg:!h-[200px] align-center ">
           {/* <PerspectiveCamera makeDefault args={[0.75, 1, 0.01, 10]} /> */}
           <directionalLight position={[0.3, 0.3, 0.3]} intensity={2}/>
