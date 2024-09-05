c7c9a708aca33099073add09556d32f8bd41c647
Dale Hutchinson <daletristanhutchinson@gmail.com>
2024-09-05 20:33:55 +1000

    more styling

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -7,8 +7,8 @@ import { Logo } from '@/Components/Logo'
 export const Header = () => {
   return (
     <section className="relative flex-col h-[60vh] lg:h-[80vh] px-0 overflow-hidden w-full mx-0 z-[-10] justify-center align-middle">
-      <div className='h-[60vh] lg:h-[80vh] w-full overflow-hidden bg-[url("/pexels-finn-whelen-6227601-6182776.jpg")] lg:bg-gray-900 bg-fixed bg-cover bg-center'>
-        <video className="fixed z-0 px-0 mx-auto w-full h-[60vh] lg:h-[80vh] bg-[60%] lg:object-cover content-center" autoPlay loop muted >
+      <div className='h-[60vh] lg:h-[80vh] w-full overflow-hidden bg-[url("/pexels-finn-whelen-6227601-6182776.jpg")] lg:bg-gray-900 bg-[size:60%] bg-fixed lg:bg-cover bg-center'>
+        <video className="fixed z-0 px-0 mx-auto w-full h-[60vh] lg:h-[80vh] object-cover content-center" autoPlay loop muted >
           <source src='17920530-uhd_3840_2160_30fps.webm' />
         </video>
         <Canvas className="absolute w-full !h-[125px] translate-y-40  mx-auto lg:translate-y-32 opacity-100 lg:!h-[200px] align-center ">
