11e31ce5af0b1218f28bd9683d0a8bba82c7e22e
Dale Hutchinson <daletristanhutchinson@gmail.com>
2024-09-06 08:07:51 +1000

    fixes for Chrome

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,9 @@ import { Footer } from '@/Components/Footer'
 
 export default function Home() {
   return (
-    <section className="w-screen mx-0 px-0">
+    <section className="w-screen min-h-screen mx-0 px-0 bg-gray-900">
       <Header />
-      <div className="relative z-[15] flex flex-col justify-center bg-gray-900">
+      <div className="relative z-[15] h-full bg-gray-900 flex flex-col justify-center">
       <About />
       <Features />
       <TeamProfiles/>
